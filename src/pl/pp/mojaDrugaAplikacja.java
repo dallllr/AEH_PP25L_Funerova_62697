a5a98bc489b271f30ec96943c90f48c124f9e480
daryna <funerovad@gmail.com>
2025-04-23 23:26:10 +0200

    Drugi zapis 2 zadanie do repozytorium

--- src/pl/pp/mojaDrugaAplikacja.java
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -7,33 +7,33 @@ public class mojaDrugaAplikacja {
         Scanner scanner = new Scanner(System.in) ;
         System.out.println("Proszę podaj swoje imię:") ;
         String forename = scanner.nextLine() ;
-        System.out.println("Proszę podaj swoje nazwisko:") ;
-        String surname = scanner.nextLine() ;
-//        scanner.close();
-        System.out.println("Witaj" + forename + " " + surname) ;
+        System.out.println("Ile masz lat?:") ;
+        String age = scanner.nextLine() ;
+        scanner.close();
+        System.out.println("Witaj " + forename + "Twój wiek w sekundach: " + Integer.parseInt(age)*60*60*24*365 + 's') ;
 
-        double x = 10;
-        double y = 2;
-        var result = x + y;
-        System.out.println("x + y = " + result);
-
-        result = x - y;
-        System.out.println("x - y = " + result);
-
-        result = x * y;
-        System.out.println("x * y = " + result);
-
-        result = x / y;
-        System.out.println("x / y = " + result);
-
-        result = x % y;
-        System.out.println("x % y = " + result);
-
-        System.out.println("Wpisz dwie liczby rozdzielone klawiszem Enter:");
-        double first = scanner.nextDouble(); //prośba o wpisanie wartości typu double
-        double second = scanner.nextDouble();
-
-        System.out.println("x + y = " + (first + second));
+//        double x = 10;
+//        double y = 2;
+//        var result = x*x + y;
+//        System.out.println("x + y = " + result);
+//
+//        result = x*x - y;
+//        System.out.println("x - y = " + result);
+//
+//        result = x*x * y;
+//        System.out.println("x * y = " + result);
+//
+//        result = x*x / y;
+//        System.out.println("x / y = " + result);
+//
+//        result = x*x % y;
+//        System.out.println("x % y = " + result);
+//
+//        System.out.println("Wpisz dwie liczby rozdzielone klawiszem Enter:");
+//        double first = scanner.nextDouble(); //prośba o wpisanie wartości typu double
+//        double second = scanner.nextDouble();
+//
+//        System.out.println("x + y = " + (first + second));
 
     }
 }
